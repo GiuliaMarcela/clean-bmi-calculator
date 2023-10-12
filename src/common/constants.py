8bfa83f35327404571b36ab6e5ed109e57785e0a
Giulia Marcela <giuliamendes67@gmail.com>
2023-10-12 19:15:55 -0300

    feat: add common constants and validations

---
+++ src/common/constants.py
@@ -0,0 +1,6 @@
+MEASUREMENTS_EXCEED_WORLD_RECORDS_MESSAGE = "Deve ser menor ou igual ao valor máximo permitido."
+WEIGHT_LESS_THAN_OR_EQUAL_TO_ZERO_MESSAGE = "Peso inválido. Deve ser maior do que zero"
+INVALID_VALUE_MESSAGE = "Valor inválido. Apenas números são aceitos"
+HEIGHT_LESS_THAN_OR_EQUAL_TO_ZERO_MESSAGE = "Altura inválida. Deve ser maior do que zero"
+MAX_METRIC_HEIGHT = 2.50
+MAX_METRIC_WEIGHT = 595.0
---
+++ src/common/input_validator.py
@@ -0,0 +1,48 @@
+from src.common.constants import (
+    HEIGHT_LESS_THAN_OR_EQUAL_TO_ZERO_MESSAGE,
+    INVALID_VALUE_MESSAGE,
+    WEIGHT_LESS_THAN_OR_EQUAL_TO_ZERO_MESSAGE
+)
+
+
+class InputValidator():
+    """
+    Classe utilitária para validar dados de peso e altura.
+
+    Essa classe fornece métodos estáticos para verificar se os valores de peso e altura são válidos.
+    Caso contrário, uma exceção 'ValueError' é lançada com uma mensagem descritiva.
+    """
+
+    @staticmethod
+    def validate_weight(weight: int):
+        """
+        Valida o peso fornecido.
+
+        Args:
+            weight(int): O valor do peso a ser validado.
+
+        Raises:
+            ValueError: Se o peso for menor ou igual a zero 
+            ou se o valor fornecido não for do tipo `int`.
+        """
+        if not isinstance(weight, (int, float)):
+            raise ValueError(INVALID_VALUE_MESSAGE)
+        if weight <= 0:
+            raise ValueError(WEIGHT_LESS_THAN_OR_EQUAL_TO_ZERO_MESSAGE)
+
+    @staticmethod
+    def validate_height(height: float):
+        """
+        Valida a altura fornecida.
+
+        Args:
+            height(float): O valor da altura a ser validado.
+
+        Raises:
+            ValueError: Se a altura for menor ou igual a zero ou 
+            se o valor informado diferente de `int` ou `float`.
+        """
+        if not isinstance(height, (int, float)):
+            raise ValueError(INVALID_VALUE_MESSAGE)
+        if height <= 0:
+            raise ValueError(HEIGHT_LESS_THAN_OR_EQUAL_TO_ZERO_MESSAGE)
