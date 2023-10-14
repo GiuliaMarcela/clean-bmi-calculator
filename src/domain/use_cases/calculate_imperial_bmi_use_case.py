250f48d4938ab379b0c4969cb2b31866be78a88e
Giulia Marcela <giuliamendes67@gmail.com>
2023-10-14 19:22:55 -0300

    feat: add imperial logic

--- src/domain/use_cases/calculate_imperial_bmi_use_case.py
+++ src/domain/use_cases/calculate_imperial_bmi_use_case.py
@@ -1,3 +1,8 @@
+from src.common.constants import (
+    MAX_IMPERIAL_WEIGHT,
+    MEASUREMENTS_EXCEED_WORLD_RECORDS_MESSAGE,
+    MAX_IMPERIAL_HEIGHT
+)
 from src.common.input_validator import InputValidator
 from src.domain.bmi_calculator import Calculator
 
@@ -16,4 +21,12 @@ class CalculateImperialBMIUseCase(Calculator):
         self.input_validator = InputValidator()
 
     def calculate(self, weight, height) -> float:
-        pass
+        self.input_validator.validate_height(height)
+        self.input_validator.validate_weight(weight)
+
+        if weight > MAX_IMPERIAL_WEIGHT:
+            raise ValueError(MEASUREMENTS_EXCEED_WORLD_RECORDS_MESSAGE)
+        if height > MAX_IMPERIAL_HEIGHT:
+            raise ValueError(MEASUREMENTS_EXCEED_WORLD_RECORDS_MESSAGE)
+
+        return round((weight / (height ** 2)) * 703, 1)
